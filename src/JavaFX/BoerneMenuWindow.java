6909df555fa4b794c5f70fb6cae74cd0818caf4b
MaskedPotato <lucasmbpoulsen@hotmail.com>
2016-03-15 10:49:26 +0100

    stadig igang med boernescenen

--- src/JavaFX/BoerneMenuWindow.java
+++ src/JavaFX/BoerneMenuWindow.java
@@ -1,4 +1,20 @@
 package JavaFX;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 public class BoerneMenuWindow {
+
+    public static Scene BoerneScene(){
+
+        BorderPane outerWindow = new BorderPane();
+
+        
+
+        Scene newScene = new Scene(outerWindow);
+
+        return newScene;
+    }
+
+
 }
