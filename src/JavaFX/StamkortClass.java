4349c9d7bfb5734b99c077366526115aba0943e9
goofy123goofy <scvalby@gmail.com>
2016-03-15 09:33:27 +0100

    tilføjet Stamkort

---
+++ src/JavaFX/StamkortClass.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * Created by svend on 15-03-2016.
+ */
+    public class StamkortClass {
+
+
+
+
+        public void stamkort(Stage primaryStage) {
+
+            VBox <StamkortClass> vbox = new VBox<>();
+
+
+
+            pane.getChildren().add(VBox);
+            Scene scene = new Scene(pane);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Stamkort");
+            primaryStage.setHeight(200);
+            primaryStage.setWidth(300);
+            primaryStage.show();
+
+        }
+
+
+
+
+    }
